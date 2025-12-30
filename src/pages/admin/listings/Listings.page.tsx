793efae4a6b03ad73d27484b8e32f3f6ff020788
kaushalya4s5s7 <chaudharikaushal02@gmail.com>
2025-12-31 02:20:33 +0530

    fix: Allow clearance announcement for assets in 'BIDDING' phase

--- src/pages/admin/listings/Listings.page.tsx
+++ src/pages/admin/listings/Listings.page.tsx
@@ -169,7 +169,7 @@ const ListingsPage = () => {
                   {new Date(asset.listing?.listedAt || asset.createdAt).toLocaleDateString()}
                 </div>
                 <div className="w-1/6 text-right">
-                  {asset.assetType === 'AUCTION' && asset.listing?.phase === 'ENDED' && (
+                  {asset.assetType === 'AUCTION' && (asset.listing?.phase === 'ENDED' || asset.listing?.phase ==='BIDDING') && (
                     <Button size="sm" onClick={() => handleEndAuctionClick(asset)} className='cta-button'>
                       Announce Clearance
                     </Button>
