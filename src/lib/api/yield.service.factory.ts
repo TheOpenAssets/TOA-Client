0282fa21bcbba9db2e4c436deecbd2e6cfd4ba4a
kaushalya4s5s7 <chaudharikaushal02@gmail.com>
2026-03-02 17:26:37 +0530

    refactor: yield factory branches on wallet type config

--- src/lib/api/yield.service.factory.ts
+++ src/lib/api/yield.service.factory.ts
@@ -4,9 +4,10 @@ import { contractService } from './contract.service';
 // @ts-ignore - stellar service might have type issues initially or if module not found
 import { stellarYieldService } from './stellar-yield.service';
 import type { NetworkType } from '../network/network.config';
+import { NETWORK_CONFIGS } from '../network/network.config';
 
 export const getYieldService = (network: NetworkType): YieldService => {
-    if (network === 'stellar') {
+    if (NETWORK_CONFIGS[network]?.walletType === 'stellar') {
         return stellarYieldService as YieldService;
     }
     return contractService as unknown as YieldService;
